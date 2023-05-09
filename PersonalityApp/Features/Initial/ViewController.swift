2cf02ec5ba8480025160638d7609b935ca471f9a
Mehmed Tukulic <mehmed.tukulic@hotmail.com>
2023-05-09 23:04:05 +0200

    initial project structure configuration

---
+++ PersonalityApp/Features/Initial/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PersonalityApp
+//
+//  Created by Mehmed Tukulic on 9. 5. 2023..
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.isNavigationBarHidden = true
+        loadInitialScreen()
+    }
+
+    private func loadInitialScreen() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) { [weak self] in
+            let vc = UIViewController()
+            vc.view.backgroundColor = .red
+            self?.navigationController?.setViewControllers([vc], animated: true)
+        }
+    }
+}
+
