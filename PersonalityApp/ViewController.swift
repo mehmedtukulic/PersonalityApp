1b41639f0a6e7e93d040a5ed7e1135689a9e2b10
Mehmed Tukulic <mehmed.tukulic@hotmail.com>
2023-05-09 22:49:14 +0200

    Initial Commit

---
+++ PersonalityApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalityApp
+//
+//  Created by Mehmed Tukulic on 9. 5. 2023..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
